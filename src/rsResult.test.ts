e75add6464b3b0ed5d823bdfa8324b27b8d320eb
lucaslos <lucas@sugestly.com>
2024-01-20 23:29:26 -0300

    Refactor error messages and metadata serialization

--- src/rsResult.test.ts
+++ src/rsResult.test.ts
@@ -29,7 +29,7 @@ test('basic functioning', () => {
   expect(divide(10, 0).unwrapOr(0)).toEqual(0);
 
   expect(() => divide(10, 0).unwrap()).toThrowErrorMatchingInlineSnapshot(
-    '"Cannot divide by zero"',
+    `[NormalizedError: Cannot divide by zero]`,
   );
 });
 
@@ -78,23 +78,23 @@ test('normalized error toString and toJSON', () => {
   expect(err.toString()).toMatchInlineSnapshot(`
     "testError: Cannot divide by zero
       Caused by: Error: Cannot divide by zero
-      Metadata: {\\"hello\\":\\"world\\"}"
+      Metadata: {"hello":"world"}"
   `);
 
   expect(String(err)).toMatchInlineSnapshot(`
     "testError: Cannot divide by zero
       Caused by: Error: Cannot divide by zero
-      Metadata: {\\"hello\\":\\"world\\"}"
+      Metadata: {"hello":"world"}"
   `);
 
   expect(JSON.stringify(err, null, 2)).toMatchInlineSnapshot(`
     "{
-      \\"id\\": \\"testError\\",
-      \\"message\\": \\"Cannot divide by zero\\",
-      \\"metadata\\": {
-        \\"hello\\": \\"world\\"
+      "id": "testError",
+      "message": "Cannot divide by zero",
+      "metadata": {
+        "hello": "world"
       },
-      \\"cause\\": \\"Error: Cannot divide by zero\\"
+      "cause": "Error: Cannot divide by zero"
     }"
   `);
 });
@@ -110,7 +110,7 @@ test('normalized error with metadata', () => {
   expect(err.toString()).toMatchInlineSnapshot(`
     "testError: Cannot divide by zero
       Caused by: Error: Cannot divide by zero
-      Metadata: \\"testMetadata\\""
+      Metadata: "testMetadata""
   `);
 
   // NormalizedErrorWithMetadata should be assignable to NormalizedError type
@@ -130,6 +130,6 @@ test('normalized error with metadata', () => {
   expect(errWithBaseErr.toString()).toMatchInlineSnapshot(`
     "base error: Base error
       Caused by: Error: Cannot divide by zero
-      Metadata: \\"appended metadata\\""
+      Metadata: "appended metadata""
   `);
 });
