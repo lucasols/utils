91fd310a3da00361b535ec261bb1e9f41e132c13
lucaslos <lucas@sugestly.com>
2024-01-20 23:44:44 -0300

    Add build:update-exports script and update dependencies

---
+++ scripts/updatePackageExports.ts
@@ -0,0 +1,34 @@
+import { readFileSync, readdir, readdirSync, writeFileSync } from 'fs';
+
+const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'));
+
+const exportedUtils: string[] = [];
+
+const srcFiles = readdirSync('./src');
+
+for (const srcFile of srcFiles) {
+  if (srcFile.endsWith('.test.ts')) {
+    continue;
+  }
+
+  exportedUtils.push(srcFile.replace(/\.ts$/, ''));
+}
+
+const newExportsField: Record<
+  string,
+  {
+    import: string;
+    types: string;
+  }
+> = {};
+
+for (const exportedUtil of exportedUtils) {
+  newExportsField[`./${exportedUtil}`] = {
+    import: `./dist/${exportedUtil}.js`,
+    types: `./dist/${exportedUtil}.d.ts`,
+  };
+}
+
+packageJson.exports = newExportsField;
+
+writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
