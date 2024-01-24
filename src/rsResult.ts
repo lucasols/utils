03b3d128020e17311247741a22db00d59a133a34
lucaslos <lucas@sugestly.com>
2024-01-24 11:29:47 -0300

    Refactor unwrap function signature

--- src/rsResult.ts
+++ src/rsResult.ts
@@ -175,9 +175,7 @@ function normalizedErr(
 }
 
 /** Unwraps a promise result */
-async function unwrap<T, E extends ResultValidErrors>(
-  result: Promise<Result<T, E>>,
-): Promise<T> {
+async function unwrap<T>(result: Promise<Result<T, any>>): Promise<T> {
   const unwrapped = await result;
 
   if (unwrapped.ok) {
