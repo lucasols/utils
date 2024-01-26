26316b3d3c2cdefc0ee68313a303f55ad0c3d2bd
lucaslos <lucas@sugestly.com>
2024-01-26 20:45:50 -0300

    Add console formatting utility

---
+++ src/consoleFmt.ts
@@ -0,0 +1,68 @@
+const colorToCodeASCII = {
+  red: '\x1b[31m',
+  green: '\x1b[32m',
+  yellow: '\x1b[33m',
+  blue: '\x1b[34m',
+  magenta: '\x1b[35m',
+  cyan: '\x1b[36m',
+  white: '\x1b[37m',
+  black: '\x1b[30m',
+  brightRed: '\x1b[91m',
+  brightGreen: '\x1b[92m',
+  brightYellow: '\x1b[93m',
+  brightBlue: '\x1b[94m',
+  brightMagenta: '\x1b[95m',
+  brightCyan: '\x1b[96m',
+  brightWhite: '\x1b[97m',
+};
+
+function consoleColors(color: keyof typeof colorToCodeASCII, text: string) {
+  return `${colorToCodeASCII[color]}${text}\x1b[0m`;
+}
+
+function bold(text: string) {
+  return `\x1b[1m${text}\x1b[0m`;
+}
+
+function underline(text: string) {
+  return `\x1b[4m${text}\x1b[0m`;
+}
+
+function italic(text: string) {
+  return `\x1b[3m${text}\x1b[0m`;
+}
+
+function strikethrough(text: string) {
+  return `\x1b[9m${text}\x1b[0m`;
+}
+
+const bgColorToCodeASCII = {
+  red: '\x1b[41m',
+  green: '\x1b[42m',
+  yellow: '\x1b[43m',
+  blue: '\x1b[44m',
+  magenta: '\x1b[45m',
+  cyan: '\x1b[46m',
+  white: '\x1b[47m',
+  black: '\x1b[40m',
+  brightRed: '\x1b[101m',
+  brightGreen: '\x1b[102m',
+  brightYellow: '\x1b[103m',
+  brightBlue: '\x1b[104m',
+  brightMagenta: '\x1b[105m',
+  brightCyan: '\x1b[106m',
+  brightWhite: '\x1b[107m',
+};
+
+function bgColor(color: keyof typeof bgColorToCodeASCII, text: string) {
+  return `\x1b[40;${bgColorToCodeASCII[color]}m${text}\x1b[0m`;
+}
+
+export const consoleFmt = {
+  color: consoleColors,
+  bold,
+  underline,
+  italic,
+  strikethrough,
+  bgColor,
+};
