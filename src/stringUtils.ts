2fdcf8e3bb5f4e6b13054f181f0882b91f433c63
lucaslos <lucas@sugestly.com>
2024-09-21 18:03:49 -0300

    refactor: remove dedent function from stringUtils to simplify code and reduce redundancy

--- src/stringUtils.ts
+++ src/stringUtils.ts
@@ -27,63 +27,6 @@ export function joinStrings(...args: (Arg | Arg[])[]) {
   return strings.join('');
 }
 
-// fork of https://github.com/dmnd/dedent
-export function dedent(strings: string): string;
-export function dedent(
-  strings: TemplateStringsArray,
-  ...values: string[]
-): string;
-export function dedent(
-  strings: TemplateStringsArray | string,
-  ...values: string[]
-) {
-  // $FlowFixMe: Flow doesn't undestand .raw
-  const raw = typeof strings === 'string' ? [strings] : strings.raw;
-
-  // first, perform interpolation
-  let result = '';
-  for (let i = 0; i < raw.length; i++) {
-    result += raw[i]! // join lines when there is a suppressed newline
-      .replace(/\\\n[ \t]*/g, '')
-      // handle escaped backticks
-      .replace(/\\`/g, '`');
-
-    if (i < values.length) {
-      result += values[i];
-    }
-  }
-
-  // now strip indentation
-  const lines = result.split('\n');
-  let mindent: number | null = null;
-  lines.forEach((l) => {
-    const m = l.match(/^(\s+)\S+/);
-    if (m) {
-      const indent = m[1]!.length;
-      if (!mindent) {
-        // this is the first indented line
-        mindent = indent;
-      } else {
-        mindent = Math.min(mindent, indent);
-      }
-    }
-  });
-
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  if (mindent !== null) {
-    const m = mindent; // appease Flow
-    result = lines.map((l) => (l.startsWith(' ') ? l.slice(m) : l)).join('\n');
-  }
-
-  return (
-    result
-      // dedent eats leading and trailing whitespace too
-      .trim()
-      // handle escaped newlines at the end to ensure they don't get stripped too
-      .replace(/\\n/g, '\n')
-  );
-}
-
 export function formatNum(num: number) {
   return num.toLocaleString('en-US', {
     minimumFractionDigits: 2,
