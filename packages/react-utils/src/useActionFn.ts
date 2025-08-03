7f68431ec75825a61aa098d42445a78d55fca5ef
lucaslos <lucas@sugestly.com>
2025-08-03 20:32:38 -0300

    feat: add useActionFn and useActionFnWithState hooks for managing asynchronous actions in React
    - Implemented `useActionFn` to handle action execution with loading state.
    - Added `useActionFnWithState` for managing actions with state-specific loading indicators.
    - Introduced utility types and functions to support the new hooks.

---
+++ packages/react-utils/src/useActionFn.ts
@@ -0,0 +1,80 @@
+import { useMemo, useRef, useState } from 'react';
+import { useConst } from './useConst';
+
+export function useActionFn<A extends unknown[], T>(
+  action: (...params: A) => T,
+): { isInProgress: boolean; call: (...args: A) => Promise<null | Awaited<T>> } {
+  const isInProgressRef = useRef(false);
+  const [isInProgress, setIsInProgress] = useState(false);
+
+  return useMemo(() => {
+    noopWithArgs(isInProgress);
+    async function call(...args: A): Promise<null | Awaited<T>> {
+      if (isInProgressRef.current) return null;
+
+      isInProgressRef.current = true;
+      setIsInProgress(true);
+
+      try {
+        const result = await action(...args);
+
+        return result;
+      } finally {
+        isInProgressRef.current = false;
+        setIsInProgress(false);
+      }
+    }
+
+    return {
+      get isInProgress() {
+        return isInProgressRef.current;
+      },
+      call,
+    };
+  }, [action, isInProgress]);
+}
+
+export function useActionFnWithState<
+  T extends string | number,
+  A extends unknown[],
+  R,
+>(
+  action: (state: T, ...params: A) => R,
+): {
+  isInProgress: (state: T) => boolean;
+  call: (state: T, ...args: A) => Promise<false | Awaited<R>>;
+} {
+  const isInProgressRef = useConst(() => new Map<T, boolean>());
+
+  const [isInProgress, setIsInProgress] = useState<PartialRecord<T, boolean>>(
+    {},
+  );
+
+  return useMemo(() => {
+    noopWithArgs(isInProgress);
+    async function call(state: T, ...args: A): Promise<false | Awaited<R>> {
+      if (isInProgressRef.get(state)) return false;
+
+      isInProgressRef.set(state, true);
+      setIsInProgress((prev) => ({ ...prev, [state]: true }));
+
+      const result = await action(state, ...args);
+
+      isInProgressRef.set(state, false);
+      setIsInProgress((prev) => ({ ...prev, [state]: false }));
+
+      return result;
+    }
+
+    return {
+      isInProgress: (state) => isInProgressRef.get(state) ?? false,
+      call,
+    };
+  }, [action, isInProgress, isInProgressRef]);
+}
+
+type PartialRecord<K extends string | number | symbol, T> = Partial<
+  Record<K, T>
+>;
+
+function noopWithArgs<T extends unknown[]>(...args: T) {}
