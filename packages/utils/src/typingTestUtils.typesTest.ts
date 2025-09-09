8bd0b7d0d75eea042bf2ec7c3067e876419a3e0f
lucasols <email@lucassantos.net>
2025-09-09 18:58:52 -0300

    refactor: improve type assertion error messages in typingTestUtils tests
    - Updated `expectTypesAreEqual` calls in `typingTestUtils.typesTest.ts` to enhance clarity of error messages.
    - Simplified the error message structure for better readability and understanding of type assertion failures.

--- packages/utils/src/typingTestUtils.typesTest.ts
+++ packages/utils/src/typingTestUtils.typesTest.ts
@@ -3,18 +3,18 @@ import { typingTest } from './typingTestUtils';
 const { expectTypesAreEqual, expectTypesAreNotEqual } = typingTest;
 
 expectTypesAreEqual<string, string>();
-expectTypesAreEqual<string, number>({
-  'error: type assertion failed, should be equal but': {
-    typeOnLeft: 'string',
-    isNotEqualTo: 2,
-  },
-});
-expectTypesAreEqual<string, { ok: string }>({
-  'error: type assertion failed, should be equal but': {
-    typeOnLeft: 'string',
-    isNotEqualTo: { ok: 'string' },
-  },
-});
+expectTypesAreEqual<string, number>(
+  'type assertion failed, types should be equal but',
+  'string',
+  'is not equal to',
+  2,
+);
+expectTypesAreEqual<string, { ok: string }>(
+  'type assertion failed, types should be equal but',
+  'string',
+  'is not equal to',
+  { ok: 'string' },
+);
 
 expectTypesAreNotEqual<string, { ok: string }>();
 
